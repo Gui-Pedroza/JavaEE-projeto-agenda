04c2f0862efc13e9843a67cb412351c6fbb0a306
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-05-07 15:15:37 -0300

    Implementação método inserirContato()

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -8,11 +8,14 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/main" })
+@WebServlet(urlPatterns = {"/main", "/insert"})
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -23,11 +26,28 @@ public class Controller extends HttpServlet {
 		String action = request.getServletPath();
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
+		} else {
+			response.sendRedirect("index.html");
 		}
 	}
 
 	// listar contatos
 	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+	
+	// adicionar contato
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response) throws IOException {		
+		
+		// adicionando as variáves ao objeto
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("email"));
+		// invocar o método inserirContato() passando como parametro o objeto contato
+		dao.inserirContato(contato);
+		// redirecionar para a página agenda.jsp
+		response.sendRedirect("/main");
 	}
 }
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -2,20 +2,20 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-import com.mysql.cj.protocol.x.SyncFlushDeflaterOutputStream;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class DAO {
-	
+
 	/* Módulo de conexão */
-	
+
 	// parametros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/agenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
 	private String password;
-	
-	// métodos de conexão
+
+	// método de conexão
 	private Connection conectar() {
 		Connection con = null;
 		try {
@@ -27,6 +27,7 @@ public class DAO {
 			return null;
 		}
 	}
+
 	// teste de conexão
 	public void testeConexão() {
 		try {
@@ -36,5 +37,24 @@ public class DAO {
 		} catch (Exception e) {
 			System.out.println(e);
 		}
+	}
+
+	// persistir os objetos no banco de dados
+	public void inserirContato(JavaBeans contato) {
+		String query = "INSERT INTO contatos (nome, fone, email) VALUES (?, ?, ?)";
+		try {
+			PreparedStatement pstm = conectar().prepareStatement(query);
+			pstm.setString(1, contato.getNome());
+			pstm.setString(2, contato.getFone());
+			pstm.setString(3, contato.getEmail());
+			// executa a query:
+			pstm.executeUpdate();
+			//encerra a conexão:
+			conectar().close();
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+
 	}
 }
