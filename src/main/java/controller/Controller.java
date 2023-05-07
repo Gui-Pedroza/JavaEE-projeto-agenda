dcbdfffa839c1d737b22779c3d689ad5f01d587f
Guilherme Pedroza <gui_pedroza@hotmail.com>
2023-05-06 22:44:27 -0300

    Formulário e validação do formulário de novo contato

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,25 +1,33 @@
 package controller;
 
+import java.io.IOException;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import model.DAO;
 
-@WebServlet(urlPatterns = {"/main"})
+@WebServlet(urlPatterns = { "/main" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	DAO dao = new DAO();
 
 	public Controller() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		String action = request.getServletPath();
+		if (action.equals("/main")) {
+			contatos(request, response);
+		}
 	}
 
+	// listar contatos
+	protected void contatos(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.sendRedirect("agenda.jsp");
+	}
 }
---
+++ src/main/webapp/agenda.jsp
@@ -0,0 +1,15 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="UTF-8">
+<title>Agenda de Contatos</title>
+<link rel="shortcut icon" href="imagens/favicon.png" type="image/x-icon">
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>Agenda de contados</h1>
+	<a href="novo.html" class="botao1">Novo contato</a>
+</body>
+</html>
